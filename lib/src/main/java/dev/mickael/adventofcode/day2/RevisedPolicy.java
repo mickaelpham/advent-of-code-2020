e62d3304db9c3ab305136e886ad8b7e7938d2da7
Mickaël Pham <inbox@mickael.dev>
2020-12-02 09:08:30 -0800

    feat: day 2 revised policy

---
+++ lib/src/main/java/dev/mickael/adventofcode/day2/RevisedPolicy.java
@@ -0,0 +1,46 @@
+package dev.mickael.adventofcode.day2;
+
+import java.util.Arrays;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class RevisedPolicy {
+
+  private final int[] positions;
+  private final char letter;
+
+  public static RevisedPolicy fromString(String input) {
+    var length = input.length();
+
+    // pick up the last character as the letter
+    var letter = input.charAt(length - 1);
+
+    // trim the last two characters, then split by `-` to get the min/max count
+    var positions =
+        Arrays.stream(input.substring(0, length - 2).split("-"))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+    assert positions.length == 2 : "expected two parsed integers";
+
+    return builder().positions(positions).letter(letter).build();
+  }
+
+  public boolean validate(String password) {
+    var count = 0;
+
+    for (var p : positions) {
+      // sanity check
+      if (password.length() < p) {
+        return false;
+      }
+
+      if (password.charAt(p - 1) == letter) {
+        count++;
+      }
+    }
+
+    return count == 1;
+  }
+}
