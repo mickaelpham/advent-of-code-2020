26439996e283a5ae80e8ed10810b4b51b316de1f
Mickaël Pham <inbox@mickael.dev>
2020-12-08 11:01:06 -0800

    fix: clean up test setup in boot program

--- lib/src/test/java/dev/mickael/adventofcode/day8/BootProgramTest.java
+++ lib/src/test/java/dev/mickael/adventofcode/day8/BootProgramTest.java
@@ -7,17 +7,31 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class BootProgramTest {
 
-  @Test
-  void testExample() throws URISyntaxException, IOException {
-    var inputPath = getClass().getClassLoader().getResource("day-8-example.txt");
-    assertNotNull(inputPath);
+  private static final String EXAMPLE_FILENAME = "day-8-example.txt";
+  private static final String INPUT_FILENAME = "day-8-input.txt";
 
-    var lines = Files.readAllLines(Path.of(inputPath.toURI()));
-    var program = BootProgram.builder().lines(lines).build();
+  private static BootProgram loadProgram(String filename) {
+    var inputPath = BootProgramTest.class.getClassLoader().getResource(filename);
+    assertNotNull(inputPath, "could not load resource: " + filename);
+
+    List<String> lines = null;
+    try {
+      lines = Files.readAllLines(Path.of(inputPath.toURI()));
+    } catch (IOException | URISyntaxException ignored) {
+    }
+    assertNotNull(lines);
+
+    return BootProgram.builder().lines(lines).build();
+  }
+
+  @Test
+  void testExample() {
+    var program = loadProgram(EXAMPLE_FILENAME);
 
     int cursor = 0;
     while (cursor != -1) {
@@ -28,12 +42,8 @@ public class BootProgramTest {
   }
 
   @Test
-  void testInputFile() throws URISyntaxException, IOException {
-    var inputPath = getClass().getClassLoader().getResource("day-8-input.txt");
-    assertNotNull(inputPath);
-
-    var lines = Files.readAllLines(Path.of(inputPath.toURI()));
-    var program = BootProgram.builder().lines(lines).build();
+  void testInputFile() {
+    var program = loadProgram(INPUT_FILENAME);
 
     int cursor = 0;
     while (cursor != -1) {
@@ -44,12 +54,8 @@ public class BootProgramTest {
   }
 
   @Test
-  void testDebugExampleFile() throws URISyntaxException, IOException {
-    var inputPath = getClass().getClassLoader().getResource("day-8-example.txt");
-    assertNotNull(inputPath);
-
-    var lines = Files.readAllLines(Path.of(inputPath.toURI()));
-    var program = BootProgram.builder().lines(lines).build();
+  void testDebugExampleFile() {
+    var program = loadProgram(EXAMPLE_FILENAME);
 
     int cursor = 0;
     while (program.debugFrom(cursor) == -1) {
@@ -60,12 +66,8 @@ public class BootProgramTest {
   }
 
   @Test
-  void testDebugInputFile() throws URISyntaxException, IOException {
-    var inputPath = getClass().getClassLoader().getResource("day-8-input.txt");
-    assertNotNull(inputPath);
-
-    var lines = Files.readAllLines(Path.of(inputPath.toURI()));
-    var program = BootProgram.builder().lines(lines).build();
+  void testDebugInputFile() {
+    var program = loadProgram(INPUT_FILENAME);
 
     int cursor = 0;
     while (program.debugFrom(cursor) == -1) {
