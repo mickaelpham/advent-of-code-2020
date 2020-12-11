6b82ce9e93bc98e7c8ef937f6cc7cd4e95b22f50
Mickaël Pham <inbox@mickael.dev>
2020-12-11 08:35:05 -0800

    fix day 9 logs

--- lib/src/test/java/dev/mickael/adventofcode/day9/TwoSumPreambleTest.java
+++ lib/src/test/java/dev/mickael/adventofcode/day9/TwoSumPreambleTest.java
@@ -8,8 +8,10 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class TwoSumPreambleTest {
 
   @Test
@@ -34,7 +36,7 @@ public class TwoSumPreambleTest {
 
       if (Arrays.equals(result, new long[] {0, 0})) {
         invalidNumber = nums[i];
-        System.out.println("first invalid number is " + invalidNumber + " at index " + i);
+        log.info("first invalid number is {} at index {}", invalidNumber, i);
       }
 
       left++;
@@ -66,7 +68,7 @@ public class TwoSumPreambleTest {
 
       if (Arrays.equals(result, new long[] {0, 0})) {
         invalidNumber = nums[i];
-        System.out.println("first invalid number is " + invalidNumber + " at index " + i);
+        log.info("first invalid number is {} at index {}", invalidNumber, i);
       }
 
       left++;
