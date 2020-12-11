8677625af9a4a8fe1ca02d9fd9eadf45e21009f2
Mickaël Pham <inbox@mickael.dev>
2020-12-10 22:19:51 -0800

    feat: day 11 seating system

---
+++ lib/src/test/java/dev/mickael/adventofcode/day11/SeatingSystemTest.java
@@ -0,0 +1,81 @@
+package dev.mickael.adventofcode.day11;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import dev.mickael.adventofcode.day11.SeatingSystem.Seat;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+public class SeatingSystemTest {
+
+  @Test
+  void testParser() {
+    var input =
+        new char[][] {
+          new char[] {'.', 'L'},
+          new char[] {'#', '.'}
+        };
+
+    var expected =
+        new Seat[][] {
+          new Seat[] {Seat.GROUND, Seat.EMPTY},
+          new Seat[] {Seat.OCCUPIED, Seat.GROUND}
+        };
+
+    var seating = SeatingSystem.fromInput(input).getGrid();
+
+    assertArrayEquals(expected, seating);
+  }
+
+  @Test
+  void testExampleFile() throws URISyntaxException, IOException {
+    var inputPath = getClass().getClassLoader().getResource("day-11-example.txt");
+    assertNotNull(inputPath);
+
+    var input =
+        Files.readAllLines(Path.of(inputPath.toURI())).stream()
+            .map(String::toCharArray)
+            .collect(Collectors.toList());
+
+    // FIXME find a better way to convert this list to an array
+    char[][] grid = new char[input.size()][input.get(0).length];
+    for (int i = 0; i < input.size(); i++) {
+      grid[i] = input.get(i);
+    }
+
+    var seating = SeatingSystem.fromInput(grid);
+    while (seating.next())
+      ;
+
+    assertEquals(37, seating.numEmpty());
+  }
+
+  @Test
+  void testInputFile() throws URISyntaxException, IOException {
+    var inputPath = getClass().getClassLoader().getResource("day-11-input.txt");
+    assertNotNull(inputPath);
+
+    var input =
+        Files.readAllLines(Path.of(inputPath.toURI())).stream()
+            .map(String::toCharArray)
+            .collect(Collectors.toList());
+
+    // FIXME find a better way to convert this list to an array
+    char[][] grid = new char[input.size()][input.get(0).length];
+    for (int i = 0; i < input.size(); i++) {
+      grid[i] = input.get(i);
+    }
+
+    var seating = SeatingSystem.fromInput(grid);
+    while (seating.next())
+      ;
+
+    assertEquals(2319, seating.numEmpty());
+  }
+}
